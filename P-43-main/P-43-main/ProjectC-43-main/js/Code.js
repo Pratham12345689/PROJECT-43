5a1910ff21ff00ff60114b1f0f345f30061f8fd9
Pratham12345689 <76481784+Pratham12345689@users.noreply.github.com>
2021-06-09 12:59:54 +0530

    Add files via upload

---
+++ P-43-main/P-43-main/ProjectC-43-main/js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    fill ("white");
+    textSize(15);
+    text("REVBAILA",100,50);
+    fill ("lightblue");
+    text ("IT IS ALWAYS CHANGING",100,70)
+
+    fill ("white");
+    textSize(15);
+    text("ATEDASBA",100,250);
+    fill ("lightblue");
+    text ("STORES ALL INFOMATION",100,270)
+
+    fill ("white");
+    textSize(15);
+    text("CUTNIFON",700,150);
+    fill ("lightblue");
+    text ("PROFERS A PERTICULAR TASK",700,170);
+    
+    
+}
